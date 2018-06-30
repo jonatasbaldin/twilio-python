d02890900e3376a90d85c9852397861c00d12cf0
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-06-30 15:06:41 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export API_DEFINITIONS_SHA=$(git log --oneline | grep Regenerated | head -n1 | cut -d ' ' -f 5)
-# make docker-build
-# make docker-push
+# export API_DEFINITIONS_SHA=$(git log --oneline | grep Regenerated | head -n1 | cut -d ' ' -f 5)
+make docker-build
+make docker-push
 
-docker build -t jonatasbaldin/twilio-python .
-docker tag jonatasbaldin/twilio-python jonatasbaldin/twilio-python:$TRAVIS_TAG
-docker tag jonatasbaldin/twilio-python jonatasbaldin/twilio-python:apidefs-$API_DEFINITIONS_SHA
+# docker build -t jonatasbaldin/twilio-python .
+# docker tag jonatasbaldin/twilio-python jonatasbaldin/twilio-python:$TRAVIS_TAG
+# docker tag jonatasbaldin/twilio-python jonatasbaldin/twilio-python:apidefs-$API_DEFINITIONS_SHA
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push jonatasbaldin/twilio-python:$TRAVIS_TAG
-docker push jonatasbaldin/twilio-python:apidefs-$API_DEFINITIONS_SHA
+# docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+# docker push jonatasbaldin/twilio-python:$TRAVIS_TAG
+# docker push jonatasbaldin/twilio-python:apidefs-$API_DEFINITIONS_SHA
